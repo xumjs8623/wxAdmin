0d885dac1540186d63af6c328fc3a44817ea1b72
mj <xumj8955@126.com>
2017-02-20 14:10:31 +0800

    123

---
+++ src/components/Admin.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="admin">
+		<p>{{msg}}</p>
+	</div>
+</template>
+<script>
+export default {
+  name: 'admin',
+  data () {
+    return {
+      msg: '111'
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from 'components/Hello'
+import Admin from 'components/Admin'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
+      name: '首页',
       component: Hello
+    },
+    {
+      path: '/admin',
+      name: '主页',
+      component: Admin
     }
   ]
 })
