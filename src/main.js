22957fbc34bd1793e1ed0334e4d3556f83dda404
mj <xumj8955@126.com>
2017-02-20 13:37:25 +0800

    123

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+// 引入element-ui组件，样式需要单独引入
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
 import App from './App'
 import router from './router'
+
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
