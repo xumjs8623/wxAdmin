77f04a7603c41f3a1b67784dc90393377880dbf5
mj <xumj8955@126.com>
2017-02-24 11:02:46 +0800

    左侧菜单栏生成

--- src/App.vue
+++ src/App.vue
@@ -9,3 +9,8 @@ export default {
   name: 'app'
 }
 </script>
+<style lang="">
+  #app{
+    margin: -8px;
+  }
+</style>
--- src/components/common/adminLeft.vue
+++ src/components/common/adminLeft.vue
@@ -1,54 +1,34 @@
 <template>
-	<div class="admin-let">
-    <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
-	</div>
-	
+  <div class="admin-let">
+    <!--<el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>-->
+    <el-menu default-active="1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
+      <el-submenu  v-for="item in menus" :index="item.index">
+        <template slot="title"><i :class="item.icon"></i>{{item.name}}</template>
+        <el-menu-item v-for="sub in item.children" :index="sub.index">{{sub.name}}</el-menu-item>
+      </el-submenu>
+    </el-menu>
+  </div>
+
 </template>
 <script>
-export default {
-  name: 'admin-left',
-  data () {
-    return {
-      data: [{
-        label: '一级菜单1',
-        url: '/menus1',
-        level: 1,
-        children: [{
-          label: '二级菜单',
-          // level: 2,
-          url: '/menus1-1'
-        }]
-      }, {
-        label: '一级菜单2',
-        url: '/menus2',
-        level: 1,
-        children: [{
-          label: '二级菜单2',
-          level: 2,
-          url: '/menus2-1'
-        }]
-      }, {
-        label: '一级菜单3',
-        url: '/menus3',
-        level: 1,
-        children: [{
-          label: '二级菜单3',
-          level: 2,
-          url: '/menus3-1'
-        }]
-      }],
-      defaultProps: {
-        children: 'children',
-        label: 'label'
+  export default {
+    name: 'admin-left',
+    data () {
+      return {
+        menus: [
+        {name: '导航一', icon: 'el-icon-information', index: '1', children: [{name: '导航1', url: '/admin/a', index: '1-1'}, {name: '导航2', url: '/admin/a', index: '1-2'}, {name: '导航3', url: '/admin/a', index: '1-3'}]},
+        {name: '导航二', icon: 'el-icon-picture', index: '2'},
+        {name: '导航三', icon: 'el-icon-message', index: '3'}
+        ]
       }
-    }
-  },
-  methods: {
-    handleNodeClick (data) {
-      if (data.hasOwnProperty('level') && data.level === 2) {
-        console.log('level=2')
+    },
+    methods: {
+      handleNodeClick (data) {
+        if (data.hasOwnProperty('level') && data.level === 2) {
+          console.log('level=2')
+        }
       }
     }
   }
-}
+
 </script>
