c61070d438347ab4f6284cc1828c267d6506e7c0
mj <xumj8955@126.com>
2017-03-01 13:10:27 +0800

    123

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import Main from 'components/content/content1'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history', // 修改路由模式，history跟原有路由风格一样
+  // mode: 'history', // 修改路由模式，history跟原有路由风格一样
   routes: [{
     path: '/',
     name: '首页',
